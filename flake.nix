91667d9957506bdb38246acbbccb2cc0ff88e3a4
user9592844 <139545179+user9592844@users.noreply.github.com>
2024-09-13 17:04:10 +0800

    Fix: Removed typo in nixos-cosmic substituters

--- flake.nix
+++ flake.nix
@@ -50,7 +50,7 @@
             { home-manager.extraSpecialArgs = specialArgs; }
             {
               nix.settings = {
-                substituters = [ "https://cosmic-cachix.org" ];
+                substituters = [ "https://cosmic.cachix.org" ];
                 trusted-public-keys = [
                   "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
                 ];
